138a071ffb990ba89a8f0629fe488610b1818df3
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-21 01:40:41 +0200

    feat: Adding a TabView

---
+++ MyWorkouts/StartTab.swift
@@ -0,0 +1,29 @@
+//
+//  StartTab.swift
+//  MyWorkouts
+//
+//  Created by Marcin Jędrzejak on 21/04/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct StartTab: View {
+    var body: some View {
+        TabView {
+            ActivityListView()
+                .tabItem {
+                    Label("Activities", systemImage: "figure.mixed.cardio")
+                }
+            Text("Calendar View")
+                .tabItem {
+                    Label("Calendar", systemImage: "calendar")
+                }
+        }
+    }
+}
+
+#Preview {
+    StartTab()
+        .modelContainer(Activity.preview)
+}
